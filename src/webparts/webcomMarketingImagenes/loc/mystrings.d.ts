3c4aa681205b9942089efaf3b47653fcf4d4f778
ccooreman <97060213+ccooreman@users.noreply.github.com>
2022-01-03 18:02:18 -0300

    first comit

---
+++ src/webparts/webcomMarketingImagenes/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWebcomMarketingImagenesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'WebcomMarketingImagenesWebPartStrings' {
+  const strings: IWebcomMarketingImagenesWebPartStrings;
+  export = strings;
+}
